745986008dec5ef43014a2c365e5cf6404f917e6
Raymond Oluoch <rasholuoch@gmail.com>
2022-02-18 22:42:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // todays forecast
-const kelvin = 273
+const kelvin = 293
 
 // getting the celsius from kelvin - 273
 const celsius = kelvin - 273
 
 // converting celsius to Fahrenheit
-let fahrenheit = Math.floor(celsius * (9/5) + 32)
+const fahrenheit = Math.floor(celsius * (9/5) + 32)
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
 
 // convert celsius to the Newton
-let newton = Math.floor(celsius * (33/100))
+const newton = Math.floor(celsius * (33/100))
 
-console.log(`The temperature is ${newton} in Newton.`)
+console.log(`The temperature is ${newton} degrees Newton.`)
