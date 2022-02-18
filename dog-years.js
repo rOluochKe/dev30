ab5f5f4c25b5690e022e3cbcc0225aea973d414e
Raymond Oluoch <rasholuoch@gmail.com>
2022-02-18 22:56:06 +0300

    Create dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,22 @@
+// my current age
+const myAge = 28
+
+// early years
+let earlyYears = 2
+
+earlyYears = earlyYears * 10.5
+
+// later years
+let laterYears = myAge - 2
+
+// later years
+laterYears = laterYears * 4
+
+// console.log(earlyYears, laterYears)
+
+// my age in dogs years
+const myAgeInDogYears = earlyYears + laterYears
+
+const myName = "Raymond".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
