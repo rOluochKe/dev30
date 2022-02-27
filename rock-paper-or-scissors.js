e8dce335bedf900366b8522df6d8df4aa11576b4
Raymond Oluoch <rasholuoch@gmail.com>
2022-02-27 17:57:20 +0300

    Update rock-paper-or-scissors.js

--- rock-paper-or-scissors.js
+++ rock-paper-or-scissors.js
@@ -1,26 +1,69 @@
 function getUserChoice(userInput) {
  userInput = userInput.toLowerCase()
- if (userInput === 'rock') {
-  return userInput = `Valid choice: ${userInput}`
- } else if (userInput === 'paper') {
-  return userInput = `Valid choice: ${userInput}`
- } else if (userInput === 'scissors') {
-  return userInput = `Valid choice: ${scissors}`
+ if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+  return userInput
  } else {
-  return userInput = 'Invalid choice, try again!'
+  return userInput = `Invalid choice, ${userInput}, try again!`
  }
 }
 
 function getComputerChoice() {
- return Math.floor(Math.random() * 2);
+ const randomNumber =  Math.floor(Math.random() * 3);
+ 
+ if (randomNumber === 0) {
+  return 'rock'
+ } else if (randomNumber === 1) {
+  return 'paper'
+ } else if (randomNumber === 2) {
+  return 'scissors'
+ }
 }
 
 function determineWinner(userChoice, computerChoice) {
  if (userChoice === computerChoice) {
-  return 'The game is a tie'
- } else if (userChoice === 'rock') {
-  return 'The computer won'
- } else if (computerChoice === 'paper') {
-  return 'the user won'
- }
+  return 'The game is a tie!'
+ } 
+ 
+ if (userChoice === 'rock') {
+  if (computerChoice === 'paper') {
+   return 'The computer won!'
+  }
+  
+  if (computerChoice === 'scissors') {
+   return 'The user won!'
+  }
+ } 
+ 
+  if (userChoice === 'paper') {
+   if (computerChoice === 'scissors') {
+    return 'The computer won!'
+   }
+   
+   if (computerChoice === 'rock') {
+    return 'The user won!'
+   }
+  }
+  
+  if (userChoice === 'scissors') {
+   if (computerChoice === 'rock') {
+    return 'The computer won!'
+   }
+   
+   if (computerChoice === 'paper') {
+    return 'The user won!'
+   }
+  }
+
 }
+
+function playGame() {
+ const userChoice = getUserChoice('scissors')
+ const computerChoice = getComputerChoice()
+ 
+ console.log(`User choice is ${userChoice}`)
+ console.log(`Computer choice is ${computerChoice}`)
+ 
+ console.log(determineWinner(userChoice, computerChoice))
+}
+
+playGame()
