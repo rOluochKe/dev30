626166bfa1b6e9425f13c67927c67fedbfda51d4
Raymond Oluoch <rasholuoch@gmail.com>
2022-03-13 15:40:45 +0300

    Update whale-talk.js

--- whale-talk.js
+++ whale-talk.js
@@ -1,4 +1,24 @@
-cont input = "whale talk"
-let vowels = []
+const input = "Building whale talk"
+const vowels = ['a', 'e', 'i', 'o', 'u']
 
 let resultArray = []
+
+for (let i = 0; i < input.length; i++) {
+ const letter = input[i].toLowerCase()
+ 
+ if (letter === "e" || letter === "u") {
+  resultArray.push[letter]
+ }
+ 
+ for (let j = 0; j < vowels.length; j++) {
+  const vowel = vowels[j]
+  
+  if (letter === vowel) {
+   resultArray.push[letter]
+  }
+ }
+}
+
+const result = resultArray.join('').toUpperCase()
+
+console.log(result)
