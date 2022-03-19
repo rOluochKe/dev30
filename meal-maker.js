1ae347d3a8a6627ffcad72cb51914a37a108206c
Raymond Oluoch <rasholuoch@gmail.com>
2022-03-19 14:01:26 +0300

    Create meal-maker.js

---
+++ meal-maker.js
@@ -0,0 +1,14 @@
+const menu = {
+ courses: {
+  appetizers: [],
+  mains: [],
+  desserts: [], 
+ },
+ addDishToCourse(courseName, dishName, dishPrice) {
+  const dish = {
+   name: 
+   price: 
+  }
+ }
+}
+
